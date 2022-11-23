aa7cbe25d49909ea95fd87be298e2430e4ce24ff
Hridoy Haque <102531674+hridoyhaque01@users.noreply.github.com>
2022-11-24 00:22:57 +0600

    some file change

--- js/main.js
+++ js/main.js
@@ -13,3 +13,25 @@ function navbarFun() {
     toggleBtn.classList.remove("bx-x");
   }
 }
+
+const scrollBtn = document.querySelector(".scrollBtn"),
+  header = document.getElementById("header");
+
+function scrollTopFun() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function showScrollBtn() {
+  const scrollTop = window.scrollY;
+  if (scrollTop > 100) {
+    scrollBtn.classList.add("show_btn");
+    header.classList.add("boxShadow");
+  } else {
+    scrollBtn.classList.remove("show_btn");
+    header.classList.remove("boxShadow");
+  }
+}
+
+window.addEventListener("load", showScrollBtn);
+window.addEventListener("scroll", showScrollBtn);
